3c67201785fc4c76e9065c9926e25974eb162daf
Oona Carbonie <carbonie.oona@outlook.com>
2019-04-22 23:53:58 -0400

    third commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
@@ -17,7 +18,7 @@ public class Main {
          without using the library function.
          */
 //        Scanner input = new Scanner(System.in);
-
+//
 //       //Ask user for string input
 //        System.out.println("Please enter a string: ");
 //        String mysterySentence = input.nextLine();
@@ -28,26 +29,65 @@ public class Main {
        If there are any duplicate values in that array, remove them and
        print out the remaining values.
        */
-       double []num = new double[10];
-       Scanner input = new Scanner(System.in);
-       System.out.println("Please enter 10 values: ");
+//       double []num = new double[10];
+//       Scanner input = new Scanner(System.in);
+//       System.out.println("Please enter 10 values: ");
+//
+//       for(int i =0; i < 10; i++) {
+//           num[i] = input.nextDouble();
+//
+//           for (int k = 0; k < 10; k++) {
+//               if (i == k) {
+//                   continue;
+//               } else if (num[i] == num[k]) {
+//                   num[i] = num[k]=0;
+//               }
+//           }
+//       }
+//       for(double numbers:num) {
+//           if (numbers != 0) {
+//               System.out.println(numbers);
+//           }
+//       }
+        /* 4)Given array 1: [1,7,6,5,9] and array 2: [2,7,6,3,4]
+        Write a program that will print out all pairs from arrays 1 and 2
+        that gives a sum of 13
+         */
+//        int [] num = {1,7,6,5,9};
+//        int [] num2 ={2,7,6,3,4};
+//
+//        for(int i = 0; i < num.length; i++) {
+//            for (int j = 0; j < num2.length; j++) {
+//                if ((num[i] + num2[j]) == 13) {
+//                    System.out.println("(" + num[i] + "," + num2[j] + ")");
+//                }
+//            }
+//        }
+        /* 5) Prompt the user to input 10 values and store them into an array.
+         Output the total number of odd and even values in the array.
+         */
+        //Declare variables
+//        int even =0;
+//        int odd = 0;
+//        int [] num = new int[10];
+//        Scanner input = new Scanner(System.in);
+//
+//        //Ask user input
+//        System.out.println("Enter 10 integers: ");
+//
+//        for(int i = 0; i < 10; i++){
+//            num[i] = input.nextInt();
+//
+//            if(num[i] % 2 == 0){
+//                even++;
+//            }
+//            else{
+//                odd++;
+//            }
+//        }
+//        System.out.println("total number of odd: "+ odd);
+//        System.out.println("Total number of even: " + even);
 
-       for(int i =0; i < 10; i++) {
-           num[i] = input.nextDouble();
-
-           for (int k = 0; k < 10; k++) {
-               if (i == k) {
-                   continue;
-               } else if (num[i] == num[k]) {
-                   num[i] = num[k]=0;
-               }
-           }
-       }
-       for(double numbers:num) {
-           if (numbers != 0) {
-               System.out.println(numbers);
-           }
-       }
 
        }
     }
